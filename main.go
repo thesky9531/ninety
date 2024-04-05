ac53746c16d6720bd7b485956851666117a8d226
thesky9531 <xinjie9531@163.com>
2024-04-05 16:49:48 +0800

    feat: add build flag

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
 	"ninety/controller"
 )
 
 func main() {
+	// 解析命令行参数
+	var port string
+	flag.StringVar(&port, "port", "8888", "HTTP service port")
+	flag.Parse()
+
 	r := gin.Default()
 	r.GET("/ws", controller.DealWss)
-	r.Run(":8888")
+	r.Run(":" + port)
 }
